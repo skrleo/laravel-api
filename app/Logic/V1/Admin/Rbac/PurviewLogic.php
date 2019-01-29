12fd46f722f8ad6a0204f66c1a2e0932994ae769
Chenguanghui <13035809409@163.com>
2019-01-30 00:45:10 +0800

    fix

--- app/Logic/V1/Admin/Rbac/PurviewLogic.php
+++ app/Logic/V1/Admin/Rbac/PurviewLogic.php
@@ -9,8 +9,11 @@
 namespace App\Logic\V1\Admin\Rbac;
 
 use App\Logic\LoadDataLogic;
+use App\Model\Exception;
 use App\Model\V1\Rbac\Purview\RoleToNodeModel;
 use App\Model\V1\Rbac\Purview\UserToRoleModel;
+use App\Model\V1\Rbac\Role\RoleModel;
+use App\Model\V1\User\UserBaseModel;
 
 class PurviewLogic extends LoadDataLogic
 {
@@ -34,8 +37,13 @@ class PurviewLogic extends LoadDataLogic
     /**
      * 添加用户角色关系
      * @return array
+     * @throws Exception
      */
     public function userToRole(){
+        $userBaseModel = (new UserBaseModel())->where('uid',$this->uid)->first();
+        if (empty($userBaseModel)){
+            throw new Exception('用户不存在','NOT_FIND_USER');
+        }
         foreach ($this->roleIds as $roleId){
             (new UserToRoleModel)->firstOrCreate([
                 'uid' => $this->uid,
@@ -48,12 +56,17 @@ class PurviewLogic extends LoadDataLogic
     /**
      * 添加角色节点关系
      * @return array
+     * @throws Exception
      */
     public function roleToNode(){
+        $roleModel = (new RoleModel())->where('role_id',$this->roleId)->first();
+        if (empty($roleModel)){
+            throw new Exception('角色不存在','NOT_FIND_ROLE');
+        }
         foreach ($this->nodeIds as $nodeId){
             (new RoleToNodeModel())->firstOrCreate([
-                'roleId' => $this->roleId,
-                'nodeId' => $nodeId
+                'role_id' => $this->roleId,
+                'node_id' => $nodeId
             ]);
         }
         return [];
