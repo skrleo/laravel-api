ba1aaf748ff02325427678eeb54317bc1cf2c3fb
Chenguanghui <13035809409@163.com>
2019-02-03 15:44:56 +0800

    定时任务

---
+++ app/Console/Crontabs/AlpacaDaemon.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chen
+ * Date: 2019/2/3
+ * Time: 15:29
+ */
+
+namespace App\Console\Crontabs;
+
+use Illuminate\Console\Command;
+
+/**
+ * 守护进程
+ * Class AlpacaDaemon
+ * @package App\Console\Crontabs
+ */
+class AlpacaDaemon extends Command
+{
+    private $daemon_json = __DIR__ . '/deamon.json';
+
+    private static $instance;
+
+    private $events = [];
+
+    public static function daemon()
+    {
+        return self::getInstance();
+    }
+
+    private static function getInstance()
+    {
+        if(!self::$instance){
+            self::$instance = new self();
+            self::$instance->daemon_json = base_path('storage') . '/crontab/deamon.json';
+        }
+        return self::$instance;
+    }
+
+    public function setDaemon($daemon_json)
+    {
+        $this->daemon_json = $daemon_json;
+        return $this;
+    }
+
+    public function setEvents(array $events)
+    {
+        $this->events = $events;
+        return $this;
+    }
+
+    public function status()
+    {
+        $data = json_decode(file_get_contents($this->daemon_json),true);
+        if(empty($data)){
+            $data = array();
+        }
+        return $data;
+    }
+
+    public function stop()
+    {
+        $data =new \stdClass();
+        $data->code="1001";
+        $data->message="Stop at:".date("Y-m-d H:i:s" ,time());
+        file_put_contents($this->daemon_json,json_encode($data),LOCK_EX);
+
+        $result["result_code"] = "1";
+        $result["result_message"] = "操作成功";
+        return $result;
+    }
+
+    public function start()
+    {
+        $data = json_decode(file_get_contents($this->daemon_json) , true);
+        if(empty($data)){
+            $data['code']="1001";
+        }
+
+        if($data['code'] == "1000" ){
+            //die("Error - exit,   Already running !");
+            return;
+        }
+
+        $data['code']="1000";
+        $data['message']="Start";
+        file_put_contents($this->daemon_json,json_encode($data),LOCK_EX);
+
+        ignore_user_abort(true);     // 忽略客户端断开
+        set_time_limit(0);           // 设置执行不超时
+
+
+        while(true){
+            $data = json_decode(file_get_contents($this->daemon_json) , true);
+            if(empty($data) || empty($data['code']) || $data['code'] == "1001" ){
+                break;
+            }
+
+            if(!empty($this->events)){
+                foreach ($this->events as $e){
+                    $e();
+                }
+            }
+
+            $data['message'] = date("Y-m-d H:i:s" ,time())." : Working ...";
+            file_put_contents($this->daemon_json, json_encode($data), LOCK_EX);
+            sleep(1);
+        }
+        $this->stop();
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
+        Commands\CrontabCommand::class,
         Commands\WorkermanCommand::class
     ];
 
