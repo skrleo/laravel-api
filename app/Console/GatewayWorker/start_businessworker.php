34c115b026ef27968a69f9fa0ad342d6acd41c51
Chenguanghui <13035809409@163.com>
2019-02-06 23:23:49 +0800

    fix

--- app/Console/GatewayWorker/start_businessworker.php
+++ app/Console/GatewayWorker/start_businessworker.php
@@ -17,9 +17,6 @@ use \GatewayWorker\Gateway;
 use \GatewayWorker\BusinessWorker;
 use \Workerman\Autoloader;
 
-// 自动加载类
-require_once __DIR__ . '../../../vendor/autoload.php';
-
 // bussinessWorker 进程
 $worker = new BusinessWorker();
 // worker名称
