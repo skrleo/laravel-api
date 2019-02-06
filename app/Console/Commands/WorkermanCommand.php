f1cf600a2c00d3dbf4915d5913d2238cce7fd05e
Chenguanghui <13035809409@163.com>
2019-02-06 23:00:57 +0800

    fix

--- app/Console/Commands/WorkermanCommand.php
+++ app/Console/Commands/WorkermanCommand.php
@@ -147,4 +147,42 @@ class WorkermanCommand extends Command
         // 启动Register服务 -- 必须是text协议
         new Register('text://0.0.0.0:' . config('gateway.register.port'));
     }
+
+    /**
+     * 当客户端连接时触发
+     * 如果业务不需此回调可以删除onConnect
+     *
+     * @param int $client_id 连接id
+     * @throws \Exception
+     */
+    public static function onConnect($client_id)
+    {
+        // 向当前client_id发送数据
+        WsSender::sendToClient($client_id, "Hello $client_id\r\n");
+        // 向所有人发送
+        WsSender::sendToAll("$client_id login\r\n");
+    }
+
+    /**
+     * 当客户端发来消息时触发
+     * @param int $client_id 连接id
+     * @param mixed $message 具体消息
+     * @throws \Exception
+     */
+    public static function onMessage($client_id, $message)
+    {
+        // 向所有人发送
+        WsSender::sendToAll("$client_id said: $message\r\n");
+    }
+
+    /**
+     * 当用户断开连接时触发
+     * @param $client_id
+     * @throws \Exception
+     */
+    public static function onClose($client_id)
+    {
+        // 向所有人发送
+        WsSender::sendToAll("$client_id logout\r\n");
+    }
 }
