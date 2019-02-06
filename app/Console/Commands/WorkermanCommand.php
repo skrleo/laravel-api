e510c2f357e8b66a580fa11f1b8e70bbd2e8af65
Chenguanghui <13035809409@163.com>
2019-02-06 23:29:23 +0800

    fix

--- app/Console/Commands/WorkermanCommand.php
+++ app/Console/Commands/WorkermanCommand.php
@@ -71,8 +71,6 @@ class WorkermanCommand extends Command
         // 标记是全局启动
         define('GLOBAL_START', 1);
 
-        require_once __DIR__ . '../../../vendor/autoload.php';
-
         // 加载所有Applications/*/start.php，以便启动所有服务
         foreach(glob(__DIR__.'../GatewayWorker/start*.php') as $start_file)
         {
