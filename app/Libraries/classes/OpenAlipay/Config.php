cc00b1cd0ef2b831cf55bccbbf146eec6d37a20f
Chenguanghui <13035809409@163.com>
2020-01-03 22:06:01 +0800

    fix

---
+++ app/Libraries/classes/OpenAlipay/Config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 支付宝配置文件
+ * User: chen
+ * Date: 2020/1/3
+ * Time: 21:28
+ */
+
+namespace App\Libraries\classes\OpenAlipay;
+
+
+class Config
+{
+
+}
---
+++ app/Libraries/classes/OpenWechat/Config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 微信配置文件
+ * User: chen
+ * Date: 2020/1/3
+ * Time: 21:27
+ */
+
+namespace App\Libraries\classes\OpenWechat;
+
+
+class Config
+{
+
+}
