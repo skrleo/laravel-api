841b740c09f48fe92aefe0d00b887baccb1fe9de
Chenguanghui <13035809409@163.com>
2020-02-02 17:43:17 +0800

    Update EnableCrossRequest.php

--- app/Http/Middleware/EnableCrossRequest.php
+++ app/Http/Middleware/EnableCrossRequest.php
@@ -21,11 +21,11 @@ class EnableCrossRequest
             'http://manage.17wangku.com',
         ];
         if (in_array($origin, $allow_origin)) {
-            $response->header('Access-Control-Allow-Origin', $origin);
-            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
-            $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
-            $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-            $response->header('Access-Control-Allow-Credentials', 'true');
+            $response->header('Access-Control-Allow-Origin', '*');
+//            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
+//            $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
+//            $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
+//            $response->header('Access-Control-Allow-Credentials', 'true');
         }
         return $response;
     }
