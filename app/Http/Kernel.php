9aa24747e46c370bb18a74d344293c9c1de3fd31
Chenguanghui <13035809409@163.com>
2020-02-02 18:11:31 +0800

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,8 +41,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
-            'bindings',
+//            'throttle:60,1',
+//            'bindings',
         ],
     ];
 
