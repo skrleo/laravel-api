23421503e7c6a8ab8111849e073ea05e0c75ff82
Chenguanghui <13035809409@163.com>
2020-02-04 00:35:50 +0800

    fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -66,7 +66,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         $route = Route::prefix('v1.0/api')
-//             ->middleware('api')
+             ->middleware('api')
              ->namespace($this->namespace);
 
         // 公共路由
